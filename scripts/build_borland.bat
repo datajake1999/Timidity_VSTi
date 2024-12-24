e2f1f10e44aad94f0ff703efcbb7a3d65e75f052
datajake1999 <datajake@gmail.com>
2024-12-23 20:57:02 -0800

    Changed order of object files in Borland and DMC link commands.

--- scripts/build_borland.bat
+++ scripts/build_borland.bat
@@ -4,5 +4,5 @@ set include=C:\BCC55\Include
 set lib=C:\BCC55\Lib
 bcc32 -q -c -DVST_FORCE_DEPRECATED=0 -DGUI -I..\include\pstdint;..\VST2_SDK -O2 -w-8004 -w-8057 @src.lst
 brcc32 -fo Timidity.res ..\res\Timidity.rc
-ilink32 -q /Tpd c0d32.obj CPU.obj editor.obj guihelp.obj lock.obj main.obj midiprog.obj Timidity.obj Timidity_proc.obj queue.obj reaper.obj common.obj filter.obj instrum.obj mix.obj playmidi.obj readcfg.obj readmidi.obj resample.obj tables.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,Timidity.dll,,cw32mt.lib import32.lib,..\res\borland.def,Timidity.res
+ilink32 -q /Tpd c0d32.obj CPU.obj editor.obj guihelp.obj lock.obj main.obj midiprog.obj queue.obj reaper.obj Timidity.obj Timidity_proc.obj common.obj filter.obj instrum.obj mix.obj playmidi.obj readcfg.obj readmidi.obj resample.obj tables.obj audioeffect.obj audioeffectx.obj vstplugmain.obj,Timidity.dll,,cw32mt.lib import32.lib,..\res\borland.def,Timidity.res
 del *.ilc *.ild *.ilf *.ils *.map *.obj *.res *.tds
