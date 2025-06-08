94ef7ab76495edb86442f05da17b0079f0d2363d
datajake1999 <datajake@gmail.com>
2025-06-07 21:58:01 -0700

    Fixed production build scripts.

--- build.bat
+++ build.bat
@@ -5,16 +5,16 @@ cd..
 cd Timidity_VSTi
 call "C:\Program Files\Microsoft Visual Studio 8\VC\bin\vcvars32.bat"
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /SRV32 /RETAIL
-cl /nologo -c /D _CRT_SECURE_NO_DEPRECATE /D VST_FORCE_DEPRECATED=0 /D GUI /I ".\include\msinttypes" /I ".\VST2_SDK" /O2 src\*.cpp timidity\*.c VST2_SDK\public.sdk\source\vst2.x\*.cpp
+cl /nologo -c /D _CRT_SECURE_NO_DEPRECATE /D VST_FORCE_DEPRECATED=0 /D GUI /D _WIN32_WINNT=0x0400 /I ".\include\msinttypes" /I ".\VST2_SDK" /O2 src\*.cpp timidity\*.c VST2_SDK\public.sdk\source\vst2.x\*.cpp
 rc res\Timidity.rc
 link *.obj res\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib gdi32.lib kernel32.lib shell32.lib user32.lib /nologo /dll /def:".\res\vstplug.def" /OUT:Timidity_X86.dll
 del *.obj
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /X64 /RETAIL
-cl /nologo -c /D _CRT_SECURE_NO_DEPRECATE /D VST_FORCE_DEPRECATED=0 /D GUI /I ".\include\msinttypes" /I ".\VST2_SDK" /O2 src\*.cpp timidity\*.c VST2_SDK\public.sdk\source\vst2.x\*.cpp
+cl /nologo -c /D _CRT_SECURE_NO_DEPRECATE /D VST_FORCE_DEPRECATED=0 /D GUI /D _WIN32_WINNT=0x0400 /I ".\include\msinttypes" /I ".\VST2_SDK" /O2 src\*.cpp timidity\*.c VST2_SDK\public.sdk\source\vst2.x\*.cpp
 link *.obj res\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib gdi32.lib kernel32.lib shell32.lib user32.lib /nologo /dll /def:".\res\vstplug.def" /OUT:Timidity_X64.dll
 del *.obj
 call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /SRV64 /RETAIL
-cl /nologo -c /D _CRT_SECURE_NO_DEPRECATE /D VST_FORCE_DEPRECATED=0 /D GUI /I ".\include\msinttypes" /I ".\VST2_SDK" /O2 src\*.cpp timidity\*.c VST2_SDK\public.sdk\source\vst2.x\*.cpp
+cl /nologo -c /D _CRT_SECURE_NO_DEPRECATE /D VST_FORCE_DEPRECATED=0 /D GUI /D _WIN32_WINNT=0x0400 /I ".\include\msinttypes" /I ".\VST2_SDK" /O2 src\*.cpp timidity\*.c VST2_SDK\public.sdk\source\vst2.x\*.cpp
 link *.obj res\Timidity.res advapi32.lib bufferoverflowU.lib comctl32.lib comdlg32.lib gdi32.lib kernel32.lib shell32.lib user32.lib /nologo /dll /def:".\res\vstplug.def" /OUT:Timidity_IA64.dll
 del *.exp *.lib *.obj
 del res\Timidity.res
