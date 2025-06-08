b8c862976c9c92c9b8c87fccc23dd477301a69eb
datajake1999 <datajake@gmail.com>
2025-06-07 22:18:31 -0700

    Updated Borland and MINGW build scripts.

--- scripts/build_mingw.bat
+++ scripts/build_mingw.bat
@@ -1,5 +1,5 @@
 @echo off
-g++ -c -DVST_FORCE_DEPRECATED=0 -DGUI -I..\VST2_SDK -O3 ..\src\*.cpp ..\timidity\*.c ..\VST2_SDK\public.sdk\source\vst2.x\*.cpp
+g++ -c -DVST_FORCE_DEPRECATED=0 -DGUI -D_WIN32_WINNT=0x0400 -I..\VST2_SDK -O3 ..\src\*.cpp ..\timidity\*.c ..\VST2_SDK\public.sdk\source\vst2.x\*.cpp
 windres ..\res\Timidity.rc resource.o
 g++ *.o -s -static -shared -ladvapi32 -lcomctl32 -lcomdlg32 -lgdi32 -lkernel32 -lshell32 -luser32 -o Timidity.dll
 del *.o
